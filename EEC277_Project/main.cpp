5a992e801343b9b5a2939f002784c1bb653618c7
Zhongyi LIN <moderato926@gmail.com>
2017-03-04 17:42:39 -0800

    Can draw sphere array

--- EEC277_Project/main.cpp
+++ EEC277_Project/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <string>
 
 // GLEW
 #define GLEW_STATIC
@@ -17,6 +18,12 @@
 #include "shader.h"
 #include "camera.h"
 
+#ifdef __APPLE__
+#define MUL 2
+#else
+#define MUL 1
+#endif
+
 // Function prototypes
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
@@ -24,23 +31,27 @@ void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void do_movement();
 
 // Window dimensions
-const GLuint WIDTH = 600, HEIGHT = 450;
+const GLuint WIDTH = 800, HEIGHT = 600;
 
-// Camera
-Camera  camera(glm::vec3(0.0f, 0.0f, 3.0f));
-GLfloat lastX  =  WIDTH  / 2.0;
-GLfloat lastY  =  HEIGHT / 2.0;
-bool    keys[1024];
+//// Camera
+//Camera  camera(glm::vec3(0.0f, 0.0f, 3.0f));
+//GLfloat lastX  =  WIDTH  / 2.0;
+//GLfloat lastY  =  HEIGHT / 2.0;
+bool keys[1024];
+//
+//// Light attributes
+//glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+//
+//// Deltatime
+//GLfloat deltaTime = 0.0f;   // Time between current frame and last frame
+//GLfloat lastFrame = 0.0f;   // Time of last frame
 
-// Light attributes
-glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
-
-// Deltatime
-GLfloat deltaTime = 0.0f;   // Time between current frame and last frame
-GLfloat lastFrame = 0.0f;   // Time of last frame
+// Spheres
+const int MAX_SPHERE_NUM = 125;
+glm::vec3 sp_pos[MAX_SPHERE_NUM];
 
 // The MAIN function, from here we start the application and run the game loop
-int main()
+int main(int argc, char const *argv[])
 {
     // Init GLFW
     glfwInit();
@@ -60,13 +71,13 @@ int main()
     }
     glfwMakeContextCurrent(window);
     
-    // Set the required callback functions
+//    // Set the required callback functions
     glfwSetKeyCallback(window, key_callback);
-    glfwSetCursorPosCallback(window, mouse_callback);
-    glfwSetScrollCallback(window, scroll_callback);
+//    glfwSetCursorPosCallback(window, mouse_callback);
+//    glfwSetScrollCallback(window, scroll_callback);
     
     // GLFW Options
-    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+//    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
     
     // Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
     glewExperimental = GL_TRUE;
@@ -86,20 +97,13 @@ int main()
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
-    glVertexAttribPointer(
-                          0,        // attribute 0
-                          2,        // size
-                          GL_FLOAT, // type
-                          GL_FALSE, // normalized?
-                          0,        // stride
-                          (void*)0  // array buffer offset
-                          );
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
     glEnableVertexAttribArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
     
-////     Define the viewport dimensions
-//    glViewport(0, 0, WIDTH, HEIGHT);
+//     Define the viewport dimensions
+    glViewport(0, 0, 2 * WIDTH, 2 * HEIGHT);
     
     // OpenGL options
 //    glEnable(GL_DEPTH_TEST);
@@ -107,9 +111,23 @@ int main()
     // Build and compile our shader program
     Shader testShader("/Users/moderato/Desktop/EEC277/EEC277_Project/EEC277_Project/test.vs", "/Users/moderato/Desktop/EEC277/EEC277_Project/EEC277_Project/test.frag");
     
+    int num_spheres = 3;
+    if(argc > 1)
+        num_spheres = atoi(argv[1]);
+//    num_spheres = 64;
+    if(num_spheres > MAX_SPHERE_NUM) {
+        fprintf(stderr, "Too many spheres!\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    for(int i = 0; i < num_spheres; i++) {
+        sp_pos[i] = glm::vec3(-3.0f + 1.5f * (i % 5), 0.5f + 1.5f * ((i % 25) / 5), 0.0f + 1.5f * (i / 25));
+        std::cout << sp_pos[i].x << ", " << sp_pos[i].y << ", " << sp_pos[i].z << std::endl;
+    }
+    
     while (!glfwWindowShouldClose(window))
     {
-
+        glfwPollEvents();
         // Clear the colorbuffer
         glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -117,16 +135,23 @@ int main()
         testShader.Use();
         glBindVertexArray(VAO);
         glUniform1f(glGetUniformLocation(testShader.Program, "iGlobalTime"), glfwGetTime());
-        glUniform3f(glGetUniformLocation(testShader.Program, "iResolution"), WIDTH, HEIGHT, 0);
-//        glDrawArrays(GL_POINTS, 0, sizeof(g_vertex_buffer_data) / 8);
+        glUniform3f(glGetUniformLocation(testShader.Program, "iResolution"), WIDTH * MUL, HEIGHT * MUL, 0);
+        glUniform1i(glGetUniformLocation(testShader.Program, "num_spheres"), num_spheres);
+        
+        for(int i = 0; i < num_spheres; i++) {
+            std::string sphere = "spheres[" + std::to_string(i) + "]";
+            glUniform1f(glGetUniformLocation(testShader.Program, (sphere + ".radius").c_str()), 0.5f);
+            glUniform3f(glGetUniformLocation(testShader.Program, (sphere + ".position").c_str()), sp_pos[i].x, sp_pos[i].y, sp_pos[i].z);
+            glUniform3f(glGetUniformLocation(testShader.Program, (sphere + ".material.color").c_str()), 0.2f, 0.3 + 0.005f * i, 0.8f);
+            glUniform1f(glGetUniformLocation(testShader.Program, (sphere + ".material.diffuse").c_str()), 1.0f);
+            glUniform1f(glGetUniformLocation(testShader.Program, (sphere + ".material.specular").c_str()), 0.5f);
+        }
+        
         glDrawArrays(GL_TRIANGLES, 0, 6);
         glBindVertexArray(0);
-        
 
-        
         // Swap the screen buffers
         glfwSwapBuffers(window);
-        glfwPollEvents();
     }
     
     // Terminate GLFW, clearing any resources allocated by GLFW.
@@ -134,7 +159,7 @@ int main()
     return 0;
 }
 
-// Is called whenever a key is pressed/released via GLFW
+//// Is called whenever a key is pressed/released via GLFW
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
 {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
@@ -147,40 +172,40 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
         keys[key] = false;
     }
 }
-
-void do_movement()
-{
-    // Camera controls
-    if (keys[GLFW_KEY_W])
-    camera.ProcessKeyboard(FORWARD, deltaTime);
-    if (keys[GLFW_KEY_S])
-    camera.ProcessKeyboard(BACKWARD, deltaTime);
-    if (keys[GLFW_KEY_A])
-    camera.ProcessKeyboard(LEFT, deltaTime);
-    if (keys[GLFW_KEY_D])
-    camera.ProcessKeyboard(RIGHT, deltaTime);
-}
-
-bool firstMouse = true;
-void mouse_callback(GLFWwindow* window, double xpos, double ypos)
-{
-    if (firstMouse)
-    {
-        lastX = xpos;
-        lastY = ypos;
-        firstMouse = false;
-    }
-    
-    GLfloat xoffset = xpos - lastX;
-    GLfloat yoffset = lastY - ypos;  // Reversed since y-coordinates go from bottom to left
-    
-    lastX = xpos;
-    lastY = ypos;
-    
-    camera.ProcessMouseMovement(xoffset, yoffset);
-}
-
-void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
-{
-    camera.ProcessMouseScroll(yoffset);
-}
+//
+//void do_movement()
+//{
+//    // Camera controls
+//    if (keys[GLFW_KEY_W])
+//    camera.ProcessKeyboard(FORWARD, deltaTime);
+//    if (keys[GLFW_KEY_S])
+//    camera.ProcessKeyboard(BACKWARD, deltaTime);
+//    if (keys[GLFW_KEY_A])
+//    camera.ProcessKeyboard(LEFT, deltaTime);
+//    if (keys[GLFW_KEY_D])
+//    camera.ProcessKeyboard(RIGHT, deltaTime);
+//}
+//
+//bool firstMouse = true;
+//void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+//{
+//    if (firstMouse)
+//    {
+//        lastX = xpos;
+//        lastY = ypos;
+//        firstMouse = false;
+//    }
+//    
+//    GLfloat xoffset = xpos - lastX;
+//    GLfloat yoffset = lastY - ypos;  // Reversed since y-coordinates go from bottom to left
+//    
+//    lastX = xpos;
+//    lastY = ypos;
+//    
+//    camera.ProcessMouseMovement(xoffset, yoffset);
+//}
+//
+//void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+//{
+//    camera.ProcessMouseScroll(yoffset);
+//}
